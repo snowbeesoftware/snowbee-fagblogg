0d70dda4465fb3bfe16ece86f2db457bfca644e4
August Lilleaas <august@snowbee.no>
2025-10-31 20:35:13 +0100

    Next.js 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,26 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import {dirname} from "path"
 import {fileURLToPath} from "url"
-import {FlatCompat} from "@eslint/eslintrc"
 import eslintConfigPrettier from "eslint-config-prettier"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const compat = new FlatCompat({
-    baseDirectory: __dirname
-})
-
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...nextCoreWebVitals,
+    ...nextTypescript,
+    // Must be at end - disables all rules that conflict with prettier
     {
         rules: {
             "@typescript-eslint/no-explicit-any": "off",
             "@typescript-eslint/no-unused-vars": "off"
         }
     },
-    // Must be at end - disables all rules that conflict with prettier
-    eslintConfigPrettier
+    eslintConfigPrettier,
+    {
+        ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+    }
 ]
 
 export default eslintConfig
